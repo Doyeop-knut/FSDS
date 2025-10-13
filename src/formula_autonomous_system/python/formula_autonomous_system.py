6005f08d5d39adf4ba9c9822657ce826eb50f2f5
Doyeop-lee <2015152@ut.ac.kr>
2025-10-13 14:42:16 +0900

    [Cone Detection] 251013 @Doyeop-knut | 자잘한 수정

--- src/formula_autonomous_system/python/formula_autonomous_system.py
+++ src/formula_autonomous_system/python/formula_autonomous_system.py
@@ -203,8 +203,8 @@ class FormulaAutonomousSystem:
             raw_predictions1 = self.model(img1_tensor)
             raw_predictions2 = self.model(img2_tensor)
 
-        rendered_img1, left_bbox, left_conf, left_label = self.camera_util._process_and_draw_detections(image1, raw_predictions1)
-        rendered_img2, right_bbox, right_conf, right_label = self.camera_util._process_and_draw_detections(image2, raw_predictions2)
+        rendered_img1, left_bbox, left_conf = self.camera_util._process_and_draw_detections(image1, raw_predictions1)
+        rendered_img2, right_bbox, right_conf = self.camera_util._process_and_draw_detections(image2, raw_predictions2)
         
         try:
             dt_cam_lidar = camera1_msg.header.stamp.to_sec() - lidar_msg.header.stamp.to_sec()
@@ -743,7 +743,7 @@ class CameraProcessor:
      # Check if predictions_np has enough columns before indexing
         if predictions_np.shape[1] < 5:
             rospy.logwarn("Predictions tensor has fewer than 5 columns. Cannot filter by confidence.")
-            return img_copy, np.array([]),  np.array([]),  np.array([])
+            return img_copy, np.array([]),  np.array([])
             #  return img_copy
      
         confidence_mask = predictions_np[:, 4] > conf_threshold
@@ -753,7 +753,7 @@ class CameraProcessor:
         # predictions_np = predictions_np[predictions_np[:, 4] > conf_threshold]
 
         if predictions_np.shape[0] == 0:
-            return img_copy, np.array([]),  np.array([]),  np.array([])  # No detections, return original image
+            return img_copy, np.array([]),  np.array([])  # No detections, return original image
             # return img_copy # No detections, return original image
 
         boxes = predictions_np[:, :4]
@@ -768,7 +768,6 @@ class CameraProcessor:
         # Given the content, it looks like x1, y1, x2, y2.
         boxes_xyxy = []
         confidence = []
-        class_id = []
         box_output = []
         for cx_norm, cy_norm, w_norm, h_norm in boxes:
             x1 = int((cx_norm - w_norm / 2) )
@@ -786,10 +785,7 @@ class CameraProcessor:
                 box_output.append([x1, y1, x2, y2])
                 # confidence = scores[i]
                 # class_id = class_ids[i] # Use the assumed class ID
-                if class_ids[i] == 0:
-                    class_label = "cone"
                 confidence.append(scores[i])
-                class_id.append(class_label)
 
                 # Draw bounding box
                 color = (0, 255, 0) # Green for bounding box
@@ -799,7 +795,7 @@ class CameraProcessor:
                 label = f"Class {class_ids[i]}: {scores[i]:.2f}"
                 cv2.putText(img_copy, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         
-        return img_copy, np.array(box_output), np.array(confidence), np.array(class_id)
+        return img_copy, np.array(box_output), np.array(confidence)
 
     def is_point_in_bbox(self, point, bbox):
         """Checks if a 2D point is inside a bounding box."""
