1ab0db503d06a30112af6dac8efd6c2054b8592c
Doyeop-knut <gunx99@a.ut.ac.kr>
2025-10-27 00:47:27 +0900

    devel, build 폴더 삭제

--- build/fs_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/FSDS/src/fs_msgs
-# Build directory: /home/user/FSDS/build/fs_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
